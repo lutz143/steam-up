8f2c8a3a53671b67a3ff2aa1bf5ea11b472296e7
JoshNatYoyo <joshuaobrien28@yahoo.com>
2023-04-06 21:22:05 -0400

    Added coding for Game.js page and api for it. Temp for now and made utils api.js

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,8 +1,36 @@
+import React, { useState, useEffect } from "react";
 import PageContainer from "../containers/PageContainer";
 import classes from "./Game.module.css";
+import SteamAPI from "steam-webapi";
 
 function Game() {
-  //TODO
+  const [gameDetails, setGameDetails] = useState(null);
+
+  useEffect(() => {
+    // Replace with steam API key
+    const apiKey = "_STEAM_API_KEY";
+
+    const appId = //; 
+
+    // Use the Steam API to get details for the specified game
+    SteamAPI.getAppDetails(appId, apiKey).then((data) => {
+      setGameDetails(data);
+    });
+  }, []);
+
+  return (
+    <PageContainer title="Game Details">
+      {gameDetails ? (
+        <div className={classes.gameDetails}>
+          <h1>{gameDetails.name}</h1>
+          <p>{gameDetails.short_description}</p>
+          <img src={gameDetails.header_image} alt={gameDetails.name} />
+        </div>
+      ) : (
+        <p>Loading game details...</p>
+      )}
+    </PageContainer>
+  );
 }
 
 export default Game;
