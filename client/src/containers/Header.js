3ee5c10288a793bd3abf45c9762a0aae2a54588e
Dan <retrotechgaming@gmail.com>
2023-04-12 20:56:53 -0400

    Updated Header styling to fix the style better

--- client/src/containers/Header.js
+++ client/src/containers/Header.js
@@ -1,13 +1,30 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 import classes from "./Header.module.css";
 
 function Header() {
   return (
     <header className={classes.header}>
-        <NavLink to='/' className={classes.title}>Steam Up</NavLink>
+      <div className={classes.containerLeft}>
+        <NavLink to="/" className={classes.title}>
+          Steam Up
+        </NavLink>
+      </div>
+      <div className={classes.containerMiddle}>
+        <input
+          type="text"
+          name="search"
+          placeholder="Search games or profiles.."
+          className={classes.search}
+        ></input>
+      </div>
+      <div className={classes.containerRight}>
+        <button className={classes.login}>
+          Login / Register
+        </button>
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;
